04be502e6ec99f0679f19be4d9e5aef10bfd6f84
DevOps\Developer <bobbanovski@gmail.com>
2017-03-04 23:33:58 +1100

    get and post actions for todo

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var db = require('./db.js'); //link to database
+var middleware = require('./middleware'); // import local file, same directory
+
 var app = express();
 var PORT = process.env.PORT || 3000; //if port variable not set, set to 3000
+var todoNextId = 4;
 
 var todos = [{
     id: 1,
@@ -18,17 +25,12 @@ var todos = [{
     isCompleted: false
 }]
 
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-
-var db = require('./db.js'); //link to database
-
-var middleware = require('./middleware'); // import local file, same directory
-
 //implement middleware
 //order is important - place above routes
 //app.use(middleware.requireAuthentication) // use for all routes
 app.use(middleware.logger);
+app.use(bodyParser.json()); //post requests will not work without this
+app.use(express.static(__dirname + '/public')); //overrides get('/)
 
 //create route for root
 app.get('/', function(req, res){ //request, response
@@ -38,16 +40,35 @@ app.get('/', function(req, res){ //request, response
 app.get('/about', middleware.requireAuthentication, function(req, res){
     res.send('About Us');
 })
-//overrides get('/)
-app.use(express.static(__dirname + '/public'));
 
+//get all todos
 app.get('/todos', function (req, res){
     res.json(todos);
 })
 
-app.get('/todos/:id', function (){
+app.get('/todos/:id', function (req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
 
+    todos.forEach(function (todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
 })
+
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    //console.log('description: ' + body.description);
+    res.json(body);
+});
 
 app.post('/users', function (req, res) {
     //filter data sent to request
